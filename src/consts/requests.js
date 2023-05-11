53a614a0ca29c4b3aa5ba26b6f94ad4bafe2535e
valeriarochab <valeria.rocha@despegar.com>
2023-05-10 21:05:41 -0300

    fix post

--- src/consts/requests.js
+++ src/consts/requests.js
@@ -1,7 +1,5 @@
-import {API_URl} from '@env';
-
 const requests = {
-    BASE_URL: API_URl,
+    BASE_URL: process.env.API_URL,
     LOGIN: '/users/login',
     SIGNUP: '/users',
     USER: '/users',
