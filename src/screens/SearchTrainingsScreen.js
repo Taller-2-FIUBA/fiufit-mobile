1ffdb09e8bd6025e3130b4fd09eafe4fb116b28f
valeriarochab <valeria.rocha@despegar.com>
2023-06-09 13:04:28 -0300

    update search

--- src/screens/SearchTrainingsScreen.js
+++ src/screens/SearchTrainingsScreen.js
@@ -1,4 +1,5 @@
-import {View, Text, TextInput, TouchableOpacity, ScrollView} from "react-native";
+import {View, Text, TextInput, TouchableOpacity, ScrollView, StyleSheet,
+} from "react-native";
 import React, {useEffect, useState} from "react";
 /* import { Searchbar } from 'react-native-paper'; */
 import {useNavigation} from "@react-navigation/native";
@@ -7,7 +8,7 @@ import {Picker} from '@react-native-picker/picker';
 import Button from "../components/Button";
 import {fiufitStyles} from "../consts/fiufitStyles";
 import {primaryColor, secondaryColor, tertiaryColor, redColor, greyColor} from "../consts/colors";
-import { FAB, IconButton, List } from 'react-native-paper';
+import { ActivityIndicator, FAB, IconButton, List, useTheme } from 'react-native-paper';
 import {UserService} from "../services/userService";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -25,6 +26,8 @@ const TrainingItem = ({value}) => {
 
 const SearchTrainingsScreen = () => {
   /* const [searchQuery, setSearchQuery] = React.useState(''); */
+  const theme = useTheme();
+
   const [notFound, setNotFound] = React.useState(false);
   const [trainingTypes, setTrainingTypes] = React.useState({});
   const [trainingType, setTrainingType] = React.useState('');
@@ -32,7 +35,8 @@ const SearchTrainingsScreen = () => {
   const [trainings, setTrainings] = React.useState([]);
   const [expandedList, setExpandedList] = useState(trainings && trainings.map(() => false));
   const [favourite, setFavourite] = useState(false);
-  const [isAthlete, setIsAthlete] = useState();
+  const [isTrainer, setIsTrainer] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const handlePress = (index) => {
     const newList = [...expandedList];
@@ -45,10 +49,16 @@ const SearchTrainingsScreen = () => {
   /* const onChangeSearch = query => setSearchQuery(query); */
 
   const handleSearch= async () => {
+    console.log('IS TRAINER: ', isTrainer);
+    setLoading(true);
     const response = await getTrainingByTypeAndDifficulty(trainingType, trainingDifficulty);
-    const trainingsFavourites = checkFavourites(response.items);
-    setTrainings(trainingsFavourites);
+    if (!isTrainer) {
+        const trainingsFavourites = await checkFavourites(response.items);
+    }
+    setTrainings(response);
     setNotFound(response?.items?.length === 0);
+    setLoading(false);
+
   };
 
   useEffect(() => {
@@ -61,12 +71,12 @@ const SearchTrainingsScreen = () => {
   }, []);
 
   useEffect(() => {
-        const isAthleteCheck = async () => {
-            const isAthlete = await AsyncStorage.getItem('@is_athlete');
-            setIsAthlete(isAthlete);
+        const isTrainerCheck = async () => {
+            const isTrainer = await AsyncStorage.getItem('@is_trainer');
+            setIsTrainer(isTrainer);
         };
     
-        isAthleteCheck();
+        isTrainerCheck();
   }, []);
 
   const checkFavourites = async (trainings) => {
@@ -144,6 +154,10 @@ const SearchTrainingsScreen = () => {
         </View>
       ))
       } */}
+      {loading ? (
+                    <ActivityIndicator size="large" color={theme.colors.secondary} style={{flex: 1}}/>
+                )
+                : <View>
       {trainings && trainings.map((training, index) => (
                 <List.Accordion
                     key={index}
@@ -154,6 +168,7 @@ const SearchTrainingsScreen = () => {
                     expanded={expandedList[index]}
                     onPress={() => handlePress(index)}
                 >
+                    {training.rating >= 0 ?<Text style={styles.ratingText}>Global training rating: {training.rating}</Text> : null}
                     <TrainingItem
                         value={training.title}
                     />
@@ -193,7 +208,7 @@ const SearchTrainingsScreen = () => {
                                         borderRadius: 5,
                                     }}/>
                         }
-                    {isAthlete && !training.favourite &&
+                    {!isTrainer && !training.favourite &&
                           <TouchableOpacity
                               style={fiufitStyles.editButton}
                               onPress={() => handleFavouriteTraining(training.id)}
@@ -206,7 +221,7 @@ const SearchTrainingsScreen = () => {
                               />
                           </TouchableOpacity>
                       }
-                      {isAthlete && training.favourite &&
+                      {!isTrainer && training.favourite &&
                         <TouchableOpacity
                             style={fiufitStyles.editButton}
                             onPress={() => handleUnfavouriteTraining(training.id)}
@@ -221,8 +236,22 @@ const SearchTrainingsScreen = () => {
                         }
                 </List.Accordion>
             ))}
+            </View>
+}
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+    ratingContainer: {
+        paddingBottom: 10,
+    },
+    ratingText: {
+        color: greyColor,
+        justifyContent: 'flex-start',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+});
 
 export default SearchTrainingsScreen
